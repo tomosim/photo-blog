9c106d34ee47b9df9b0a3cb66b7d8e2a1d817991
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2020-02-21 17:09:15 +0000

    create template based pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+const pages = []
+
+exports.onCreateNode = ({ node, getNode }) => {
+  if (node.internal.type === `ImageSharp`) {
+    const fileNode = getNode(node.parent)
+    const pageName = fileNode.relativePath.split("/")[1]
+    if (!pages.includes(pageName)) {
+      pages.push(pageName)
+    }
+  }
+}
+exports.createPages = ({ actions: { createPage } }) => {
+  pages.forEach(page => {
+    createPage({
+      path: page,
+      component: path.resolve(`./src/templates/photo-series.js`),
+      context: { slug: page },
+    })
+  })
+}
---
+++ src/templates/photo-series.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+function BlogPostTemplate(props) {
+  return (
+    <>
+      <h1>hello</h1>
+      <p>{props.path}</p>
+    </>
+  )
+}
+export default BlogPostTemplate
