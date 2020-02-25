015e87575c6995c3eb8e9ed3e4c03e7a1d93b100
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2020-02-25 14:40:19 +0000

    query and display images in photo-series

--- src/templates/photo-series.js
+++ src/templates/photo-series.js
@@ -1,12 +1,35 @@
 import React from "react"
 import { graphql } from "gatsby"
+import PhotoList from "../components/PhotoList"
+import styles from "./photo-series.module.css"
+import Header from "../components/Header"
 
-function BlogPostTemplate(props) {
+function PhotoSeriesTemplate({ data }) {
   return (
-    <>
-      <h1>hello</h1>
-      <p>{props.path}</p>
-    </>
+    <main className={styles.grid}>
+      <Header />
+      <PhotoList photos={data.allFile.edges} />
+    </main>
   )
 }
-export default BlogPostTemplate
+
+export default PhotoSeriesTemplate
+
+export const query = graphql`
+  query GetImages($pathRegex: String!) {
+    allFile(filter: { relativePath: { regex: $pathRegex } }) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            id
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
