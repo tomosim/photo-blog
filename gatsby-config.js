8d2f230c4db78c5f429fe63fba24067e56ac178f
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2020-02-25 17:09:02 +0000

    change meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Where the hell is Tom?`,
+    description: `Follow Tom one roll at a time`,
+    author: `@tomosim`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
